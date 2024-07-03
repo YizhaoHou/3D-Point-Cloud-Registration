1fe7f1dfa079962805daf41783a3bbdc92a49379
YZ <yzhou1612@gmail.com>
2024-07-03 16:37:36 +0800

    add down_sample and gain fpfh

--- utilities.py
+++ utilities.py
@@ -29,7 +29,6 @@ def icpRegistration(source, target, threshold = 1e-5, iteration = 2000):
     """
     apply ICP registration
 
-
     Parameters:
     source (base points)
     target (points need to be moved)
@@ -43,3 +42,36 @@ def icpRegistration(source, target, threshold = 1e-5, iteration = 2000):
     o3d.pipelines.registration.TransformationEstimationPointToPoint(),
     o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=iteration))
     return reg_p2p
+
+
+def GicpRegistration(source, target, threshold = 1e-5, iteration = 2000):
+    """
+    apply GICP registration
+    
+    Parameters:
+    source (base points)
+    target (points need to be moved)
+
+    Returns:
+    reg_p2p
+    """
+    trans_init = np.eye(4)
+    reg_p2p = o3d.pipelines.registration.registration_icp(
+    source, target, threshold, trans_init,
+    o3d.pipelines.registration.TransformationEstimationForGeneralizedICP(),
+    o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=iteration))
+    return reg_p2p
+
+def preprocess_point_cloud(pcd, voxel_size):
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+ 
+    radius_normal = voxel_size * 2
+    pcd_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=100))
+    radius_feature = voxel_size * 5
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(pcd_down, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+
+
+
+
